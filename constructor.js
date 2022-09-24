7bdb6721ae0403f0761c9e91e8164e023f8d79c7
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-25 01:01:33 +0530

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,9 @@
+function EmployeeInfo(name,age,designation,yop){
+    this.name=name;
+    this.age=age;
+    this.designation=designation;
+    this.yop=yop;
+}
+
+//var Employee1 = new EmployeeInfo("Abhijeet",24,"ASE",2);
+//Employee1.name;
